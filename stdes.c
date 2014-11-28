b9e147187e22f1d415b2a7e17805df552bf5a99f
gauchya <antho2gauchy@gmail.com>
2014-11-28 08:32:15 +0100

    ecrire et lire ok

--- stdes.c
+++ stdes.c
@@ -1,6 +1,6 @@
 #include "stdes.h"
 
-#define TAILLE 1024
+#define TAILLE 64
 
 struct _ES_FICHIER {
 	int fd;
@@ -33,11 +33,23 @@ FICHIER* ouvrir(char *nom, char mode) {
 	return NULL;
 }
 
+int flush(FICHIER *f, int taille) {
+	int somme = 0;
+	while(somme != taille) {
+		int ret = 0;
+		ret = write( f->fd,  f->buffer + somme, taille - somme);
+		if(ret == -1) {
+			return ret;
+		}
+		somme += ret;
+	}
+	
+	return somme;
+}
+
 int fermer(FICHIER* f) {
 	if(f->mode == 'E') {
-		/** 
-		TODO flush
-		 */
+		flush(f, f->index);
 	}
 	
 	return close(f->fd);
@@ -47,33 +59,36 @@ int lire(void *p, unsigned int taille, unsigned int nbelem, FICHIER *f) {
 	if(f->mode != 'L') {
 		return -1;
 	}
-	int elem_lu = 0;
+	int taille_totale_a_lire = nbelem * taille;
+	int nb_elem_lu = 0;
+	int decalage = 0;
 	
-	do  {
-		if(f->count - f->index < nbelem*taille ) {
-			int return_read = read(f->fd, (void*)(f->buffer + f->count), TAILLE - f->count);
-			if(return_read == -1) {//ERREUR
+	while (taille_totale_a_lire > f->index - f->count) {
+		// /!\ Il faut lire et mettre dans le buffer avant la premiere lecture
+		if (f->index - f->count == 0) {
+			nb_elem_lu = read(f->fd, f->buffer, TAILLE);
+			if (nb_elem_lu == -1) {	// Erreur
 				return -1;
-			}else if(return_read == 0  && f->count - f->index == 0) {//On a atteint EOF et on a plus rien a donner dans le buffer
+			} else if (nb_elem_lu == 0) {	// Fin de fichier
 				return 0;
 			}
-			f->count += return_read;
-		}
-		
-		if(f->index + taille*nbelem < TAILLE) {
-		
+			f->index = nb_elem_lu;
+			f->count = 0;
 		} else {
-		
+			memcpy(p+decalage, f->buffer + f->count, f->index - f->count);
+			taille_totale_a_lire -= f->index - f->count;
+			decalage += f->index - f->count;
+			f->count = f->index;
 		}
-		
-	}while(f->index == 0);	
+	}
 	
-	return elem_lu;
-}
-
-int flush(FICHIER *f) {
-
-	return 0;
+	if (taille_totale_a_lire <= f->index - f->count) {
+		memcpy(p, f->buffer + f->count, taille_totale_a_lire);
+		f->count += taille_totale_a_lire;
+		decalage += taille_totale_a_lire;
+	}
+	
+	return decalage;
 }
 
 int ecrire(const void *p, unsigned int taille, unsigned int nbelem, FICHIER *f) {
@@ -85,7 +100,7 @@ int ecrire(const void *p, unsigned int taille, unsigned int nbelem, FICHIER *f) 
 			//Remplir le buffer a max
 			memcpy(f->buffer+f->index, ((char *)p)+decalage, TAILLE - f->index);
 			//Flush
-			if(flush(f) == -1 ){
+			if(flush(f, TAILLE) == -1 ){
 				return -1;
 			}
 			//Augmenter le decalage, restaurer l'index, reduire taille_totale
@@ -93,11 +108,12 @@ int ecrire(const void *p, unsigned int taille, unsigned int nbelem, FICHIER *f) 
 			decalage += TAILLE - f->index;
 			f->index = 0;
 		}
-		if(taille_totale < TAILLE - f->index) {//suffisament de place pour mettre dans le buffer d'écriture
-			memcpy(f->buffer + f->index, p, taille_totale);
-		
+		if(taille_totale <= TAILLE - f->index) {//suffisament de place pour mettre dans le buffer d'écriture
+			memcpy(f->buffer + f->index, p, taille_totale);		// /!\ demander si memcpy merde
+			f->index += taille_totale;
 		}
 		
+		return nbelem * taille; // !!!! Penser a demander
 	}
 	
 	return -1;
